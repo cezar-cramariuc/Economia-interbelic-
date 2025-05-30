0428b9d54ad9cea770ed137be99952b10074976c
cezar-cramariuc <37026039+cezar-cramariuc@users.noreply.github.com>
2025-05-30 20:08:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Economia interbelică/title>
+    <link href="stil/stil.css" rel="stylesheet">
 </head>
 
 <body>
